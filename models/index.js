698144dc592bc0fb2af8afd6066d32507502ecfc
ccaitano <cheryl.caitano@gmail.com>
2022-09-07 18:21:44 -1000

    Update models index.js file with model relations

--- models/index.js
+++ models/index.js
@@ -1,5 +1,26 @@
 const User = require('./User');
+const Moods = require('./Moods');
+const Questions = require('./Questions');
+const Responses = require('./Responses');
 
 // Create associations
+User.hasMany(Responses, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
-module.exports = { User };
+Responses.belongTo(User, {
+    foreignKey: 'user_id'
+});
+
+Responses.belongTo(Questions, {
+    foreignKey: 'question_id'
+});
+
+Questions.belongTo(Moods, {
+    foreignKey: 'mood_id'
+});
+
+
+
+module.exports = { User, Moods, Questions, Responses };
