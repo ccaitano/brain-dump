9c184f6b94bcc911dcaeabdf59a418bcb89aee07
musaliyah <asmusaliar@gmail.com>
2022-09-10 10:54:26 -0700

    userData changed

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,27 +1,28 @@
-[
-  {
-    "name": "Sal",
-    "email": "sal@hotmail.com",
-    "password": "password12345"
-  },
-  {
-    "name": "Lernantino",
-    "email": "lernantino@gmail.com",
-    "password": "password12345"
-  },
-  {
-    "name": "Amiko",
-    "email": "amiko2k20@aol.com",
-    "password": "password12345"
-  },
-  {
-    "name": "Jordan",
-    "email": "jordan99@msn.com",
-    "password": "password12345"
-  },
-  {
-    "name": "Blake",
-    "email": "the_blake@yahoo.com",
-    "password": "password12345"
-  }
+const sequalize = require('../config/connection');
+const {User, Response} = require('../models');
+
+const userData = [
+    {
+        username: 'musaliyah',
+        email: 'asmusaliar@gmail.com',
+        password: 'inlandEmpire'
+    },
+    {
+        username: 'walterWhite123',
+        email: 'walterwhite@hotmail.com',
+        password: 'HeisenbergLives!'
+    },
+    {
+        username: 'jessePinkman123',
+        email: 'jessepinkman@aol.com',
+        password: '123456780'
+    },
+    {
+        username: 'teddyfreddy',
+        email: 'tfreddy@yahoo.com',
+        password: 'kibble123'
+    }
 ]
+
+const seedUser = () => User.bulkCreate(userdata);
+module.exports = seedUser;
